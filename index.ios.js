85823f09bad4e1c1626b0f9abdc79f13973a6ba5
Abdulhakim Haliru <hakimkal@gmail.com>
2017-06-12 12:34:07 +0100

    first commit

---
+++ index.ios.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class WeCodeApp extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to Wecode App!
+        </Text>
+         
+        <Text style={styles.instructions}>
+          Wecode is a community of tech enthusiasts.
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('WeCodeApp', () => WeCodeApp);
